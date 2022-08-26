2470b2afc6937b40a6d0eead4f7d67f01c2523ad
Akhil T Cheriyan <34876178+AkhilATC@users.noreply.github.com>
2022-08-26 11:22:13 +0530

    Update app.py

--- ui-impl/app.py
+++ ui-impl/app.py
@@ -14,22 +14,22 @@ def login():
 @app.route('/gen_report', methods=['POST'])
 def gen_report():
     clinic_name = request.form.get('clinic-name') or None
-    clinic_logo = request.form.get('clinic-logo')
+    clinic_logo = request.form.get('clinic-logo') or None
 
     # physical's info
-    physician_name = request.form.get('physician-name')
-    physician_contact = request.form.get('physician-contact')
+    physician_name = request.form.get('physician-name') or None
+    physician_contact = request.form.get('physician-contact') or None
 
     # patient's info
 
-    patient_fname = request.form.get('p-name')
-    patient_lname = request.form.get('p-lname')
+    patient_fname = request.form.get('p-name') or None
+    patient_lname = request.form.get('p-lname') or None
 
-    patient_dob = request.form.get('p-dob')
-    patient_contact = request.form.get('p-contact')
+    patient_dob = request.form.get('p-dob') or None
+    patient_contact = request.form.get('p-contact') or None
     # notes
-    c_note = request.form.get('input-message')
-    con_note = request.form.get('input-message-2')
+    c_note = request.form.get('input-message') or None
+    con_note = request.form.get('input-message-2') or None
     check_list = [clinic_name, physician_name, physician_contact, patient_fname,
                   patient_lname, patient_dob, patient_contact, c_note, con_note]
     print(check_list)
