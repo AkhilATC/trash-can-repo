506477b6417af583cdcae91d59233ed9a4af5e16
AkhilATC <akhilt@dexlock.com>
2022-09-16 20:26:04 +0530

    core imp - pandas filterout

--- annual-report-app/controller/data_interface.py
+++ annual-report-app/controller/data_interface.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+import pandas as pd
 
 data_layer = Blueprint('data_layer', __name__, url_prefix="/data")
 
@@ -9,12 +10,20 @@ def fetch_annual_data():
 
     :return:
     """
-    args = request.args.get('well')
-    if args:
-
+    well_id = request.args.get('well')
+    if well_id:
+        # read xlx file
+        file_ = r'data/input.xls'
+        df = pd.read_excel(file_)
+        df.columns = [c.replace(' ', '_') for c in df.columns]
+        # filter out based on input well id
+        filtered_data = df.loc[df['API_WELL__NUMBER'] == int(well_id)]
+        oil_sum = int(filtered_data['OIL'].sum()) if filtered_data['OIL'].sum() else 0
+        gas_sum = int(filtered_data['GAS'].sum()) if filtered_data['GAS'].sum() else 0
+        brine_sum = int(filtered_data['BRINE'].sum()) if filtered_data['BRINE'].sum() else 0
         return jsonify({
-            'oil': 1,
-            'gas': 1,
-            'brine': 1
+            'oil': oil_sum,
+            'gas': gas_sum,
+            'brine': brine_sum
         })
     return jsonify({'message':"can't fetch data invalid inputs"}), 400
